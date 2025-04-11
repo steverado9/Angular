0c7491711743a3dfb28bba3c38b8243a6b916346
stephen <isaacsteverado9@gmail.com>
2025-04-11 18:51:42 +0100

    made new changes

--- src/app/componets/task-item/task-item.component.ts
+++ src/app/componets/task-item/task-item.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
   @Input() task!: Task; //task! means you are telling angular to chill you know what you are doing.
+
+  faTimes = faTimes;
 }
--- src/app/componets/tasks/tasks.component.ts
+++ src/app/componets/tasks/tasks.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
- 
+import { TaskService } from '../../services/task.service'; 
+
 @Component({
   selector: 'app-tasks',
   imports: [CommonModule, TaskItemComponent],
@@ -11,5 +11,13 @@ import { TaskItemComponent } from '../task-item/task-item.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {} //added the TaskService as a provider inside the constuctor, so we can use it
+
+  ngOnInit(): void {
+    //we use it inside here cos this fires off immediately
+    this.taskService.getTasks().subscribe((tasks) => this.
+    tasks = tasks);
+  }
 }
