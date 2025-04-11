df9784674c458318a488eb91b1dae8d6aaabd29e
stephen <isaacsteverado9@gmail.com>
2025-04-11 02:05:22 +0100

    angular

---
+++ src/app/componets/task-item/task-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  @Input() task!: Task; //task! means you are telling angular to chill you know what you are doing.
+}
